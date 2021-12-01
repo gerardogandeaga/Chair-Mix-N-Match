1e197a2c1f9826cd50dee2ecdbaf5692d8e9d718
Abilly-wang <abilly0726@gmail.com>
2021-12-01 01:31:59 -0800

    driver

--- driver.py
+++ driver.py
@@ -8,14 +8,15 @@ from parser.draw3dobb import showGenshape
 # from parser.grassdata import GRASSDataset
 from parser.parser import SimpleObj, Model
 import pprint
+from mixer import mixer
 
 if __name__ == "__main__":
     # ------------------- Parser -------------------------
     # 66, 4037, 2177
     # 6012, 3453, 3336, 2234, 1392
     # 653, 18, 734, 1791, 2207, 3663, 4903, 2428, 722, 4294
-    models = Model.load_models([66, 4037, 2177, 3453]) # you should be passing the output here into the mixer
-
+    models = Model.load_models([6012, 3453, 3336, 2234, 1392]) # you should be passing the output here into the mixer
+   
     for i,model in enumerate(models):
         parts = model.components
 
@@ -27,13 +28,14 @@ if __name__ == "__main__":
         # 2.
         # You can use this nifty save function from the SimpleObj class
         # Note: the save function takes in a type of SimpleObj
-        # SimpleObj.save("back-{}".format(i), parts["objs"]["back"]) # this will save the back component of a chair
+        # SimpleObj.save("back-{}".format(i), parts["objs"]["legs"]) # this will save the back component of a chair
         
         # 3.
         # You can also merge SimpleObjs like this...
-        # back_and_seat_obj = SimpleObj.merge_objs([parts["objs"]["back"], parts["objs"]["seat"]]) # this will merge the back and seat SimpleObjects
-        # SimpleObj.save("back-and-seat-{}".format(i), back_and_seat_obj)
+        #back_and_seat_obj = SimpleObj.merge_objs([parts["objs"]["back"], parts["objs"]["seat"]]) # this will merge the back and seat SimpleObjects
+        #SimpleObj.save("back-and-seat-{}".format(i), back_and_seat_obj)
 
     # ------------------- Mixer -------------------------
     # the the function below hasnt been created yet, make sure to put your mixer logic in the .mixer/ folder 
     # mixer_result = Mixer.create_new_chair(models)
+    mixer.random_choose(models)
