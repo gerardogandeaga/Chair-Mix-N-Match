df02ce81e67cf8d939c55e4daa135d2577b5cf97
Abilly-wang <abilly0726@gmail.com>
2021-12-01 01:14:22 -0800

    simple-merge

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import scipy.io
 from parser.parser import load_models, SimpleObj
 import pprint
 
-pprint.pprint(load_models([3]))
+#pprint.pprint(load_models([3]))
 # print("PMI:", scipy.io.loadmat("./dataset/part mesh indices/3.mat"))
 # print("Lab:", scipy.io.loadmat("./dataset/labels/3.mat"))
 # print("BBs:", scipy.io.loadmat("./dataset/boxes/1.mat"))
