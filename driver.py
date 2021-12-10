9d7b2cd6e35186f62319407328586d46343d61a7
Gerardo Gandeaga <gerardogandeaga@gmail.com>
2021-12-10 15:11:07 -0800

    Moved example and test chairs to their own dir in the repo. Updated the driver.py code

--- driver.py
+++ driver.py
@@ -1,50 +1,104 @@
 import numpy as np
-import torch
 import math
 import pprint
-import copy
-from matplotlib.pyplot import box
-from parser.draw3dobb import showGenshape
-# from parser.grassdata import GRASSDataset
-from parser.parser import SimpleObj, Model
-import pprint
+import random
+import os
 
+from parser.parser import SimpleObj, Model
 from mixer.test_mix import test_mix
 from mixer.mixer import random_choose
+from scorer.renderer import generate_views
+from scorer.scorer import score_model
+
+OUT_DIR = "./output"
+OUT_OBJ_PATH = os.path.join(OUT_DIR, "new_chair")
 
 if __name__ == "__main__":
     # ------------------- Parser -------------------------
-    # 66, 4037, 2177
-    # 6012, 3453, 3336, 2234, 1392
-    # 653, 18, 734, 1791, 2207, 3663, 4903, 2428, 722, 4294
+    example_chairs_symh_id = {
+        "A": [66, 4037, 2177],
+        "B": [6012, 3453, 3336, 2234, 1392],
+        "C": [653, 18, 734, 1791, 2207, 3663, 4903, 2428, 722, 4294]
+    }
+    final_chairs_symh_id = { # THIS SET IS USED FOR THE PRESENTATION
+        "A": [369, 175, 5540],
+        "B": [2999, 2150, 3492, 4474, 2160],
+        "C": [1919, 3366, 3521, 3204, 1131, 173, 3749, 2313, 5117, 1920]
+    }
 
-    models = Model.load_models([2177]) # you should be passing the output here into the mixer
+    # TODO: ABBY and ROSSS
+    # choose some random models from the sets, however you like.
+    # you can use random.sample(list, N) to select N items from a list.
+    # random.sample(final_chairs_symh_id["C"], 4) <- returns a list of 4 random ids (without repition) from the C set
+    # you can implement however you like
+    symh_ids = random.sample(final_chairs_symh_id["C"], 4)
 
-    for i,model in enumerate(models):
-        parts = model.components
+    # ======================== PARSER ========================
+    print("Reference models chosen: {} (symh_ids)".format(symh_ids))
+    print("Loading models...")
+    models = Model.load_models(symh_ids)
+    # ======================== PARSER ========================
 
-        # 1.
-        # all you should care about from the Models class is the .components variable as that will contain all 
-        # part Objs and centers for transformations
-        # pprint.pprint(model.components) 
+    # ======================== MIXER =========================
+    print("Creating a new chair...")
+    # TODO: ABBY and ROSS
+    # use the models variable to pass into your mixer.
+    # Mixer.mix(models, OUT_OBJ_PATH)
+    # ======================== MIXER =========================
 
-        # 2.
-        # You can use this nifty save function from the SimpleObj class
-        # Note: the save function takes in a type of SimpleObj
+    # ====================== RENDERER ========================
+    # create the 3 view images
+    print("Generating TOP, SIDE and FRONT view renders...")
+    front_path, side_path, top_path = os.path.join(OUT_DIR, "front.png"), os.path.join(OUT_DIR, "side.png"), os.path.join(OUT_DIR, "top.png")
+    generate_views(OUT_OBJ_PATH, front_path, side_path, top_path)
+    # ====================== RENDERER ========================
 
-        # SimpleObj.save("back-{}".format(i), parts["objs"]["back"]) # this will save the back component of a chair
+    # ======================= SCORER =========================
+    # print if the chair generated is probable or not
+    print("Scoring chair...")
+    probable = score_model(top_path, front_path, side_path)
+    if probable == 0:
+        print("Not a probable chair")
+    else:
+        print("Probable chair")
+    print("Output chair -> '{}'".format(OUT_OBJ_PATH))
+    # ======================= SCORER =========================
 
-        # 3.
-        # You can also merge SimpleObjs like this...
-        # back_and_seat_obj = SimpleObj.merge_objs([parts["objs"]["back"], parts["objs"]["seat"]]) # this will merge the back and seat SimpleObjects
-        # SimpleObj.save("back-and-seat-{}".format(i), back_and_seat_obj)
-        # SimpleObj.save("back-{}".format(i), parts["objs"]["back"]) # this will save the back component of a chair
+    # ======================== END ===========================
 
-    # ------------------- Mixer -------------------------
-    # the the function below hasnt been created yet, make sure to put your mixer logic in the .mixer/ folder 
-    # mixer_result = Mixer.create_new_chair(models)
+    # for i,model in enumerate(models):
+    #     parts = model.components
 
+    #     # 1.
+    #     # all you should care about from the Models class is the .components variable as that will contain all 
+    #     # part Objs and centers for transformations
+    #     # pprint.pprint(model.components) 
 
-    test_mix( models )
-    random_choose( models )
+    #     # 2.
+    #     # You can use this nifty save function from the SimpleObj class
+    #     # Note: the save function takes in a type of SimpleObj
+
+    #     # SimpleObj.save("back-{}".format(i), parts["objs"]["back"]) # this will save the back component of a chair
+
+    #     # 3.
+    #     # You can also merge SimpleObjs like this...
+    #     # back_and_seat_obj = SimpleObj.merge_objs([parts["objs"]["back"], parts["objs"]["seat"]]) # this will merge the back and seat SimpleObjects
+    #     # SimpleObj.save("back-and-seat-{}".format(i), back_and_seat_obj)
+    #     # SimpleObj.save("back-{}".format(i), parts["objs"]["back"]) # this will save the back component of a chair
+
+    # # ------------------- Mixer -------------------------
+    # # the the function below hasnt been created yet, make sure to put your mixer logic in the .mixer/ folder 
+    # # mixer_result = Mixer.create_new_chair(models)
+
+    # # SYMH ids
+    # # initial test objs
+    # # 66, 4037, 2177
+    # # 6012, 3453, 3336, 2234, 1392
+    # # 653, 18, 734, 1791, 2207, 3663, 4903, 2428, 722, 4294
+    # # presentation test objs
+
+    # models = Model.load_models([2177]) # you should be passing the output here into the mixer
+
+    # test_mix( models )
+    # random_choose( models )
 
